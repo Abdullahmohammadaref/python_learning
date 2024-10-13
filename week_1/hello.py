991f8454bfaf97c27d7d523037d16bd9c83a982d
Abdullahmohammadaref <abdullahmohammadaref@gmail.com>
2024-10-13 19:21:32 +0300

    update

--- week_1/hello.py
+++ week_1/hello.py
@@ -1,2 +1,15 @@
-print ("hello world")
-print ("hi world")
+def main():
+    user_answere = input().strip().lower()
+    print(check(user_answere))
+
+def check(answere):
+    match answere:
+        case "42" | "forty-two" | "forty two" :
+            return False
+        case _:
+            return False
+
+main()
+
+
+    
