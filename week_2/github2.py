11050634a24674adcda593a2657e8861af78752d
Abdullahmohammadaref <abdullahmohammadaref@gmail.com>
2024-10-17 15:19:24 +0300

    Merge remote-tracking branch 'origin/main'

--- week_2/github2.py
+++ week_2/github2.py
@@ -118,6 +118,8 @@ print(x + y, x - y, x / y, x % y, x * y, x ** y, x // y)
 # - Replace the second item
 # - delet it again
 
+############ ai code:
+
 # Create tuple, list, and set
 my_list = ['one', 2, 'three', [1, 2, 3, 4, 5], 5]
 my_tuple = ('one', 2, 'three', (1, 2, 3, 4, 5), 5)
@@ -146,3 +148,34 @@ del my_set
 print("List:", my_list)
 print("Tuple:", my_tuple)
 print("Set:", my_set)
+
+##################### my code: only sets are problem
+
+my_list = ['one', 2, 'three', [1, 2, 3, 4, 5], 5]
+my_tuple = ('one', 2, 'three', (1, 2, 3, 4, 5), 5)
+my_set = {'one', 2, 'three', 5}
+
+my_list.append(6)
+my_tuple += (6,)
+my_set.add('six')
+
+print(my_list)
+print(my_tuple)
+print(my_set)
+
+my_list.pop(1)
+my_tuple = my_tuple[:1] + my_tuple[2:]
+my_set.remove(5)
+
+print(my_list)
+print(my_tuple)
+print(my_set)
+
+my_list[1] = 12
+my_tuple = my_tuple[:1] + (12,) + my_tuple[2:]
+my_set.remove('six')
+my_set.add(12)
+
+print(my_list)
+print(my_tuple)
+print(my_set)
