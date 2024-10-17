9efb1e02425018366dea14107c688cc2e5448469
Abdullahmohammadaref <abdullahmohammadaref@gmail.com>
2024-10-17 15:18:35 +0300

    Merge remote-tracking branch 'origin/main'

--- week_2/session_practice_1.py
+++ week_2/session_practice_1.py
@@ -4,7 +4,7 @@ my_set = {'one', 2, 'three', 5}
 
 my_list.append(6)
 my_tuple += (6,)
-my_set.add(6)
+my_set.add('six')
 
 print(my_list)
 print(my_tuple)
@@ -12,7 +12,7 @@ print(my_set)
 
 my_list.pop(1)
 my_tuple = my_tuple[:1] + my_tuple[2:]
-my_set.remove(2)
+my_set.remove(5)
 
 print(my_list)
 print(my_tuple)
@@ -20,7 +20,7 @@ print(my_set)
 
 my_list[1] = 12
 my_tuple = my_tuple[:1] + (12,) + my_tuple[2:]
-my_set.remove(2)
+my_set.remove('six')
 my_set.add(12)
 
 print(my_list)
