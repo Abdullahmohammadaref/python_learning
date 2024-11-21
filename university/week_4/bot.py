c1e4ec7cba86d7229e75747c200e9f531cf8cc1d
Abdullahmohammadaref <abdullahmohammadaref@gmail.com>
2024-11-21 16:17:25 +0300

    pizza and sourcegify done

---
+++ university/week_4/bot.py
@@ -0,0 +1,10 @@
+import myClassExample
+
+# Create an object with a specific constructor
+myObject = myClassExample.helperClass("Abdullah")
+
+# Use the functions provides by the class
+myObject.hi()
+myObject.bye()
+
+print("The name is", myObject.getName())
