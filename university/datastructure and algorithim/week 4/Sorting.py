324ccc680b49798ee6219ccf9086f4233c6ca35e
Abdullahmohammadaref <abdullahmohammadaref@gmail.com>
2024-12-08 05:10:05 +0300

    any thing

--- university/datastructure and algorithim/week 4/Sorting.py
+++ university/datastructure and algorithim/week 4/Sorting.py
@@ -1,8 +1,30 @@
+counting sort mk:
 import numpy as np
-import cv2
-import matplotlib.pyplot as plt
 
-original_array = np.array([9,5,2,4,2,8,5])
-cumlative_array = np.zeros(len(max(original_array)))
-print(cumlative_array)
+original_array = np.array([9, 5, 2, 4, 2, 8, 5], dtype = int)
+
+count_array = np.zeros(np.max(original_array) + 1, dtype=int) #[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+for number in original_array: #[0, 0, 2, 0, 1, 2, 0, 0, 1, 1]
+    count_array[number] += 1
+
+increment_array = np.array([], dtype=int)
+total = 0
+index = 0
+for number in (count_array):  # [0 0 2 0 3 5 0 0 6 7]
+    if number <= 0:
+        increment_array = np.append(increment_array, 0)
+        index += 1
+        continue
+    else:
+        increment_array = np.append(increment_array, number + total)
+        total += count_array[index]
+        index += 1
+
+sorted_array = np.zeros_like(original_array) #[0, 0, 0, 0, 0, 0, 0]
+for number in original_array:
+    increment_array[number] -= 1
+    sorted_array[increment_array[number]] = number
+
+print(sorted_array)
+
 
