dc4b1fc9d82443c2442c03448b7342fa228c397c
Abdullahmohammadaref <abdullahmohammadaref@gmail.com>
2024-12-10 14:49:37 +0300

    any thing

---
+++ university/datastructure and algorithim/week 4/practice 1.py
@@ -0,0 +1,23 @@
+top = 0
+if top != capacity - 1
+    return False
+else:
+    return True
+
+
+
+
+def IsFull()
+    if front == 0 and rear == capacity - 1:
+        return True
+    else:
+        return False
+    endif
+
+
+def IsEmpty()
+    if front == rear + 1:
+        return True
+    else:
+        return False
+    endif
